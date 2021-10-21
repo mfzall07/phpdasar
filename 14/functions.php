545d0e367f801f973c823069a6938ee4d06d831f
Rias Gauri Nurhasanah <19102212@ittelkom-pwt.ac.id>
2021-10-21 08:05:42 +0700

    register

--- 14/functions.php
+++ 14/functions.php
@@ -129,4 +129,39 @@
         return query($query);
     }
 
+    function register($data) {
+        global $conn;
+
+        $username = strtolower(stripslashes($data["username"]));
+        $password = mysqli_real_escape_string($conn, $data["password"]);
+        $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+        //username sudah ada atau belum
+        $result = mysqli_query($conn, "SELECT username FROM user WHERE username= '$username'");
+
+        if( mysqli_fetch_assoc($result)) {
+            echo "<script>
+                    alert ('Username Sudah Tersedia'); 
+                  </script>";
+            return false;
+        }
+
+        //cek konfirmasi password
+        if( $password !== $password2) {
+            echo "<script> 
+                    alert('Password Tidak Sesuai');
+                  </script>";
+            return false;
+        }
+
+        //enkripsi password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        // menambahkan user baru
+        mysqli_query($conn, "INSERT INTO user VALUES('','$username','$password')");
+
+        return mysqli_affected_rows($conn);
+    }
+    
+
 ?>
